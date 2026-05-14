1003006d014cbcfd7efdcb396afd47d787c7c990
duduufogaca <eduardofogaca.mp@gmail.com>
2026-05-15 09:31:52 +1000

    Add files via upload

--- plainvest-next/lib/stripe.ts
+++ plainvest-next/lib/stripe.ts
@@ -7,5 +7,7 @@ export function createStripeClient() {
     throw new Error('Missing STRIPE_SECRET_KEY.');
   }
 
-  return new Stripe(secretKey);
+  return new Stripe(secretKey, {
+    apiVersion: '2024-06-20',
+  });
 }
